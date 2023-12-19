526535eae338249ddf56b13b4c0f8e08e2563ab3
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-12-19 12:46:56 +0100

    Aggiungere i file di progetto.

---
+++ Pacchetti_vacanze/Pacchetto_base.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacchetti_vacanze
+{
+    internal class Pacchetto_base
+    {
+        private string _destinazione;
+        private int _durata;
+        private float _costoBase;
+        private float _costoVendita;
+
+        public string Destinazione
+        {
+            get { return _destinazione; } 
+            set { _destinazione = value; }
+        }
+
+        public int Durata
+        {
+            get { return _durata; }
+            set { if (value > 0) { _durata = value; } else { _durata = 0; } }
+        }
+
+        public float CostoBase
+        {
+            get { return _costoBase; }
+            set { if (value > 0) { _costoBase = value; } else { _costoBase = 0; } }
+        }
+
+        public float CostoVendita
+        {
+            get { return _costoVendita; }
+            set { if (value > 0) { _costoVendita = value; } else { _costoVendita = 0; } }
+        }
+
+        public Pacchetto_base() 
+        {
+            Destinazione = "";
+            Durata = 0;
+            CostoBase = 0;
+            CostoVendita = 0;
+        }
+
+        public Pacchetto_base(string destinazione, int durata, float costoBase)
+        {
+            Destinazione = destinazione;
+            Durata = durata;
+            CostoBase = costoBase;
+            CostoVendita = CalcolaPrezzo();
+        }
+
+        public float CalcolaPrezzo()
+        {
+            return CostoBase;
+        }
+
+        //metodi equals
+        public bool Equals(Pacchetto_base cmp)
+        {
+            if (Destinazione == cmp.Destinazione && Durata == cmp.Durata && CostoBase == cmp.CostoBase && CostoVendita == cmp.CostoVendita)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Override di Equals
+        public override bool Equals(object obj)
+        {
+            // Controllo se oggetto è un pacchetto base
+            if (obj is Pacchetto_base)
+                return Equals((Pacchetto_base)obj);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Destinazione.GetHashCode() + Durata.GetHashCode() + CostoBase.GetHashCode() + CostoVendita.GetHashCode();
+        }
+
+        //overrride di string
+        public override string ToString()
+        {
+            return Destinazione + " " + Durata + " " + CostoBase + " " + CostoVendita;
+        }
+    }
+}
